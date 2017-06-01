d07993e371fba75db29ff7dd306ab4880dde0094
Bill Wang <squigglydouche@gmail.com>
2017-06-01 12:47:48 +1000

    STABLE VERSION
    got errors printing

--- Viewing_Geometry.m
+++ Viewing_Geometry.m
@@ -1,17 +1,14 @@
 % Takes in the Height of the Satellite and the Nadir Angle = FOV/2
 % and returns the distance D from the satellite to the target (swath edge)
-function output = Viewing_Geometry(H, lambda)
+function output = Viewing_Geometry(H, n)
 R_e = 6378137;  %meters
 
 rho = asin(R_e/(R_e+H));
 lambda0 = acos(R_e/(R_e+H));
 
-n = atan((sin(rho)*sin(lambda))/(1-sin(rho)*cos(lambda)));
-
 E = acos(sin(n)/sin(rho));
 
-% a simple check to see if it works
-shouldbezero = n + lambda + E - pi/2;
+lambda = pi/2 - n - E;
 
 D = R_e*(sin(lambda)/sin(n));
 
