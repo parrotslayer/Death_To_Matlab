d0f544337d0505ff3d11a6303548cf530cf90a92
Bill Wang <squigglydouche@gmail.com>
2017-08-22 12:47:26 +1000

    added images and functions

--- AMME4710_Ass1/Q1_21_08.m
+++ AMME4710_Ass1/Q1_21_08.m
@@ -15,30 +15,53 @@
 % I = s*g
 % b = Ax    ...  x = A^-1 b  or  A^-1\b
 
+close all
+clear all
+clc
+
 load('facedata_yaleB07.mat')
 
 [h,w,N] = size(im_array);
 
 A = light_dirs;
 
-for i = 1:h
-    
+albedo = zeros(h,w);
+normal = zeros(h,w,3);
+
+for i = 1:h   
     for j = 1:w
-        
      
-    b = reshape(im_array(i,j,:),[N,1]);  % 1x1x64 -> 64x1
-    x = A\(double(b )/255);
+    b = reshape(im_array(i,j,:),[N,1]);     % 1x1x64 -> 64x1
+    x = A\(double(b)/255);                  % Intensity b (uint8) scaled to [0-1]
     
-    rho(i,j) = norm(x);
-    nhat = x/rho(i,j);
-    New(i,j,1) = nhat(1);
-    New(i,j,2) = nhat(2);
-    New(i,j,3) = nhat(3);
+    %compute albedo and normals
+    albedo(i,j) = norm(x);
+    nhat = x/albedo(i,j);
+    normal(i,j,1) = nhat(1);    %x
+    normal(i,j,2) = nhat(2);    %y
+    normal(i,j,3) = nhat(3);    %z
     
+    %compute p and q
+    p(i,j) = normal(i,j,1)/normal(i,j,2);
+    q(i,j) = normal(i,j,2)/normal(i,j,3);
+        
     end
-    
 end
 
+%check if (dp/dy - dq/dx)^2 is small
+
+%calculate integral
+height_map = cumsum(p,1) + cumsum(q,1);
+
+% set 1st pixel to 0
+offset = height_map(1,1);
+
+%offset array
+%height_map = height_map - offset;
+
+display_face_model(albedo, height_map)
+
+
 figure
-imagesc(rho)
+imagesc(albedo)
 colormap(gray)
