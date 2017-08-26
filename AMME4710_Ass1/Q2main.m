44ac7d336d96320f1eef82b7594fbbb02fa979bc
Bill Wang <squigglydouche@gmail.com>
2017-08-26 12:39:22 +1000

    L green working

--- AMME4710_Ass1/Q2main.m
+++ AMME4710_Ass1/Q2main.m
@@ -2,7 +2,9 @@ clc
 clear all
 close all
 
-lego = imread('legomontage.png');
+lego = imread('legomontage3.png');
+%lego = imread('legobricks012.jpg');
+
 
 figure
 imshow(lego)
@@ -10,7 +12,7 @@ title('Image Loaded')
 
 % Apply Filter
 %[BW,maskedRGBImage] = Im1_Blue(lego);
-[BW,maskedRGBImage] = Montage_Red(lego);
+[BW,maskedRGBImage] = Montage_LGreen2(lego);
 
 figure
 imshow(BW)
@@ -18,3 +20,19 @@ imshow(BW)
 figure
 imshow(maskedRGBImage)
 title('Filtered Image')
+
+%% Get info on regions
+min = 500;
+max = 20000;
+BW2 = bwareafilt(BW,[min,max]);
+figure
+imshow(BW2)
+
+BW3 = bwareafilt(BW,1);
+figure
+imshow(BW3)
+
+%BW2 = bwareafilt(BW,1);
+
+
+regions_filt = regionprops(BW3, 'Centroid', 'Area');
