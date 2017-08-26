068d10e9db6ee9b07f4878c51e8fe952934f7dc1
Bill Wang <squigglydouche@gmail.com>
2017-08-26 15:58:09 +1000

    fixed a bug with bwimfilt

--- AMME4710_Ass1/Q2_main_V2.m
+++ AMME4710_Ass1/Q2_main_V2.m
@@ -4,7 +4,14 @@ close all
 
 % Load the image
 %lego = imread('legomontage3.png');
-filename = 'legobricks001.jpg';
+lego_num = ['001';'002';'003';'004';'005';'006';'007';'008';'009';'010';...
+    '011';'012';'013';'014';'015';'016';'017'];
+%choose the image to display
+N = 1;
+
+for N = 1:17
+
+filename = ['legobricks',lego_num(N,:),'.jpg'];
 lego = imread(filename);
 
 %Preallocate arrays
@@ -26,7 +33,7 @@ max = 20000;
 BW2 = bwareafilt(BW,[min,max]);
 
 % Take largest block (no conflicts)
-BW3 = bwareafilt(BW,1);
+BW3 = bwareafilt(BW2,1);
 
 % Details on the final block
 regions_Red = regionprops(BW3, 'Centroid', 'Area', 'BoundingBox');
@@ -57,7 +64,7 @@ max = 20000;
 BW2 = bwareafilt(BW,[min,max]);
 
 % Take largest block (no conflicts)
-BW3 = bwareafilt(BW,1);
+BW3 = bwareafilt(BW2,1);
 
 % Details on the final block
 regions_DGreen = regionprops(BW3, 'Centroid', 'Area', 'BoundingBox');
@@ -88,7 +95,7 @@ max = 20000;
 BW2 = bwareafilt(BW,[min,max]);
 
 % Take largest block, high confidence only wanted block is there
-BW3 = bwareafilt(BW,1);
+BW3 = bwareafilt(BW2,1);
 
 % Details on the final block
 regions_Blue = regionprops(BW3, 'Centroid', 'Area', 'BoundingBox');
@@ -120,7 +127,7 @@ max = 20000;
 BW2 = bwareafilt(BW,[min,max]);
 
 % Take largest block (no conflicts)
-BW3 = bwareafilt(BW,1);
+BW3 = bwareafilt(BW2,1);
 
 % Details on the final block
 regions_LGreen = regionprops(BW3, 'Centroid', 'Area', 'BoundingBox');
@@ -150,7 +157,7 @@ max = 20000;
 BW2 = bwareafilt(BW,[min,max]);
 
 % Take several largest blocks cos can be lgreen, yellow or orange
-BW3 = bwareafilt(BW,1);
+BW3 = bwareafilt(BW2,1);
 
 % Details on the final block
 regions_Yellow = regionprops(BW3, 'Centroid', 'Area','BoundingBox');
@@ -169,4 +176,6 @@ if n > 0
     rectangle('Position',[box(1),(box(2)),box(3),box(4)],'LineWidth',2,'EdgeColor','r');
     string = 'Yellow';
     text(box(1),box(2)+100,string,'Color','White','FontSize',14)
+end
+
 end
