16bc29aca4f59f331c3d282d10c1119a09a69add
Bill Wang <squigglydouche@gmail.com>
2017-08-27 21:14:32 +1000

    found all the boxes

--- AMME4710_Ass1/Q2_main_PartB.m
+++ AMME4710_Ass1/Q2_main_PartB.m
@@ -17,15 +17,16 @@ I = 1;
 Yes_Colour = zeros(17,6);
 Yes_Center = zeros(17,6);
 Yes_Box = zeros(17,6);
-True_Pos = zeros(17,6);
+True_Pos = 0;
 True_Pos_Col = zeros(17,6);
 False_Neg = zeros(17,1);
 False_Pos = zeros(17,1);
 Joined_Centers = zeros(2,2);
 Joined_Boxes = zeros(2,4);
+foundboxes = 0;
 
 %% Begin looping for all images
-for I = 1:5
+for I = 1:15
 
 filename = ['bricksjoined',lego_num(I,:),'.jpg'];
 lego = imread(filename);
@@ -196,64 +197,66 @@ end
 %% Check for adjactent blocks
 [gimme,sleep,J] = size(All_boxes);
 
-% Check for Dark Green with Light Green
-plotbox = 0;
-
 % i indexes the dark green
+plotbox = zeros(1,4);
+ind = 0;
 for i = 1:J
-    %k indexes light green 
+    %k indexes yellow
     for k = 1:J
         %check area of dark green box vs light green boxes
-        area = rectint(All_boxes(2,:,i),All_boxes(4,:,k));
+        area = rectint(All_boxes(2,:,i),All_boxes(5,:,k));
         if area > 0 
             %Store two centroids of joined blocks
-            center_av(1) = (All_centroids(2,1,i)+All_centroids(4,1,k))/2;
-            center_av(2) = (All_centroids(2,2,i)+All_centroids(4,2,k))/2;
+            center_av(1) = (All_centroids(2,1,i)+All_centroids(5,1,k))/2;
+            center_av(2) = (All_centroids(2,2,i)+All_centroids(5,2,k))/2;
 
-            distance = sqrt( (All_centroids(2,1,i)-All_centroids(4,1,k))^2 + (All_centroids(2,2,i)-All_centroids(4,2,k))^2  );
+            distance = sqrt( (All_centroids(2,1,i)-All_centroids(5,1,k))^2 + (All_centroids(2,2,i)-All_centroids(5,2,k))^2  );
             
             % Find the quadrant we are in. Lgreen wrt Dgreen
-            deltaX = All_centroids(4,1,k) - All_centroids(2,1,i);
-            deltaY = -(All_centroids(4,2,k) - All_centroids(2,2,i));
+            deltaX = All_centroids(5,1,k) - All_centroids(2,1,i);
+            deltaY = -(All_centroids(5,2,k) - All_centroids(2,2,i));
             % atan2 is messy because of the image axis being flipped
             angle = abs(tan(deltaY/deltaX));
             
-            plotbox(1) = center_av(1) - 3*distance;
-            plotbox(2) = center_av(2) - 3*distance;
-            plotbox(3) = 6*distance+distance;
-            plotbox(4) = 6*distance+distance;
- 
-            %Create bounding box
-            if length(plotbox) > 1
-                
-                rectangle('Position',[plotbox(1)+3*distance,plotbox(2)+3*distance,5,5],'LineWidth',5,'EdgeColor','blue');
-                string = 'Lego Bricks Center';
-                text(plotbox(1)+3*distance,plotbox(2)+3*distance-20,string,'Color','White','FontSize',14)
-                
-                rectangle('Position',[plotbox(1),plotbox(2),plotbox(3),plotbox(4)],'LineWidth',2,'EdgeColor','red');
-                string = 'Lego Bricks Big Bad Box';
-                text(plotbox(1),plotbox(2)-20,string,'Color','White','FontSize',14)
-            end
-            
+            ind = ind + 1;
+            plotbox(ind,1) = center_av(1) - 3*distance;
+            plotbox(ind,2) = center_av(2) - 3*distance;
+            plotbox(ind,3) = 6*distance+distance;
+            plotbox(ind,4) = 6*distance+distance;
+
         end
     end    
 end
 
-% Check for Dark Green with Yellow
-if sum(sum(Joined_Boxes)) == 0
-    for i = 1:J
-        for k = 1:J
-            %check area of dark green box vs yellow boxes
-            area = rectint(All_boxes(2,:,i),All_boxes(5,:,k));
-            if area > 0
-                %Store two centroids of joined blocks
-                Joined_Centers(1,:) = All_centroids(2,:,i);
-                Joined_Boxes(1,:) = All_boxes(2,:,i);
-                Joined_Centers(2,:) = All_centroids(5,:,i);
-                Joined_Boxes(2,:) = All_boxes(5,:,i);
-            end
+%check if good block size
+threshU = 400;
+threshL = 150;
+check = 0;
+[num_boxes,killme] = size(plotbox);
+if num_boxes > 1
+    for f = 1:num_boxes
+        %check if the box is of resonable size
+        if plotbox(f,3) > threshL && plotbox(f,3) < threshU
+            ind = f;
+            check = 1;
         end
     end
+% only 1 box, plot the only one    
+else
+    ind = 1;
+    check = 1;
+end
+%Create bounding box
+if check == 1
+
+    rectangle('Position',[plotbox(ind,1)+3*distance,plotbox(ind,2)+3*distance,5,5],'LineWidth',5,'EdgeColor','blue');
+    string = 'Lego Bricks Center';
+    text(plotbox(ind,1)+3*distance,plotbox(ind,2)+3*distance-20,string,'Color','White','FontSize',14)
+
+    rectangle('Position',[plotbox(ind,1),plotbox(ind,2),plotbox(ind,3),plotbox(ind,4)],'LineWidth',2,'EdgeColor','red');
+    string = 'Lego Bricks Big Bad Box';
+    text(plotbox(ind,1),plotbox(ind,2)-20,string,'Color','White','FontSize',14)
+    foundboxes = foundboxes + 1;
 end
 
 %% Validation of data
@@ -518,17 +521,8 @@ end
 
 %% print out stats
 
-truepos = sum(sum(True_Pos))/17/6*100;
-disp(['True Positives = ', num2str(truepos),' %'])
+truepos = foundboxes/15*100;
+disp(['Found Boxes = ', num2str(truepos),' %'])
 
-trueposcol = sum(sum(True_Pos_Col))/17/6*100;
-disp(['True Positives But Colour Wrong = ', num2str(trueposcol),' %'])
-
-falsepos = sum(sum(False_Pos))/17/6*100;
-disp(['False Positives = ', num2str(falsepos),' %'])
-
-falseneg = sum(sum(False_Neg))/17/6*100;
-disp(['False Negatives = ', num2str(falseneg),' %'])
-
-leftovers = 100 - truepos - trueposcol - falsepos - falseneg;
-disp(['Fully Bricked (leftovers) = ', num2str(leftovers),' %'])
+leftovers = 100 - truepos;
+disp(['Not found blocks feelsbadman :spoon = ', num2str(leftovers),' %'])
